e944542017b1c0ff980dba4cfa1bc9f93eea975e
Pradyumna Krishna <51270439+PradyumnaKrishna@users.noreply.github.com>
2021-05-15 09:38:15 +0530

    remove legacy chatting from home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,23 +1,18 @@
 <template>
-  <ul id="messages">
-    <li v-for="message in messages" :key="message">
-      {{ message }}
-    </li>
-  </ul>
-  <form v-on:submit.prevent="onSubmit" class="form">
-    <div class="container input-group w-50 mb-3">
-      <input type="text" class="form-control" v-model="message" placeholder="Message">
-      <div class="input-group-append">
-        <button class="btn btn-primary" @click="send">Send</button>
-      </div>
-    </div>
-  </form>
+  <div class="container about w-50 mx-auto">
+    <h1>Engima Protocol</h1>
+    <br />
+    <p>
+      This Website is used for anonymous chatting with end to end encyrption support.
+      I can't decrypt those message because they are encrypted using client side of
+      the web application.
+    </p>
+    <br />
+    <button class="btn btn-primary"><a href="chat">Start Chatting</a></button>
+  </div>
 </template>
 
 <script>
-import io from "socket.io-client";
-var fetch = require('node-fetch');
-var socket = io.connect("http://localhost:5000");
 
 export default {
   name: "Home",
@@ -30,51 +25,8 @@ export default {
       message: ""
     };
   },
-  methods: {
-    login: async function() {
-      const response = await fetch('http://localhost:5000/login');
-      const json = await response.json();
-      this.user = json.user;
-      this.$cookies.set("user", this.user);
-    },
-
-    connect: async function(user) {
-      const response = await fetch(`http://localhost:5000/connect/${user}`);
-      const json = await response.json();
-      if (json.status) {
-        this.user = this.$cookies.get("user");
-      } else {
-        await this.login();
-      }
-    },
-
-    send: function() {
-      socket.send(this.message);
-      this.message = "";
-    },
-  },
-  async mounted() {
-    if (this.$cookies.isKey("user")) {
-      await this.connect(this.$cookies.get("user"))
-    } else {
-      await this.login()
-    }
-
-    var self = this;
-
-    socket.on("connect", function () {
-      socket.send(`${self.user} has connected!`);
-    });
-
-    socket.on("message", function (msg) {
-      self.messages.push(msg);
-      console.log("Received message");
-    });
-
-    socket.on("disconnect", function () {
-      socket.send(`${self.user} has disconnected!`);
-    });
-  },
+  mounted() {
+  }
 };
 </script>
 
