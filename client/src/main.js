19bf5da8c83c9060c4398ee7160fcf610d776e09
Pradyumna Krishna <51270439+PradyumnaKrishna@users.noreply.github.com>
2021-05-27 06:36:45 +0530

    Title, Favicon Changed
    Minor Changes
    - Remove Store
    - run npm audit fix

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import VueCookies from 'vue3-cookies'
 
-createApp(App).use(VueCookies).use(store).use(router).mount('#app')
+createApp(App).use(VueCookies).use(router).mount('#app')
