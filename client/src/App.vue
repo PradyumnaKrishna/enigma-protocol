8950d30f9a487eaf43f503c6b0700692188f65cf
Pradyumna Krishna <51270439+PradyumnaKrishna@users.noreply.github.com>
2021-06-12 18:24:00 +0530

    remove about and add docs

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/chat">Chat</router-link>
+    <router-link to="/chat">Chat</router-link> |
+    <router-link to="/docs">Docs</router-link>
   </div>
 
   <section class="section">
---
+++ client/src/views/Docs.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container about w-50 mx-auto">
+    <h1>Docs</h1>
+    <br/>
+    <p>
+      Hello Friends, I have built an end to end encrypted messenger
+      using Flask, SocketIO, and Vue.js. So, Here is my roadmap to achieve it.
+    </p>
+  </div>
+
+</template>
