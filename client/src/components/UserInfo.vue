0afc3f823dce3ddbafd86a09ab3d39e6f28001bb
Mattia Beccari <103275021+Tizzz-555@users.noreply.github.com>
2023-10-11 20:01:31 +0200

    style: added style to the user info component
    - copy and pasted relevant style from chat.vue
    - added underline and hover effect to the user id
    - added "clickableUserId" class to <strong> to use cursor:pointer only on the actual id

--- client/src/components/UserInfo.vue
+++ client/src/components/UserInfo.vue
@@ -2,7 +2,12 @@
   <div>
     <p id="userId" class="inputBox">
       User ID :
-      <strong @click="copy(user)" title="click to copy">{{ user }}</strong>
+      <strong
+        class="clickableUserId"
+        @click="copy(user)"
+        title="click to copy"
+        >{{ user }}</strong
+      >
     </p>
     <ToastMessage :toastmsg="toastmsg" :toastType="toastType" />
     <form v-on:submit.prevent="onSubmit" class="form">
@@ -29,7 +34,7 @@ import ToastMessage from "../components/ToastMessage.vue";
 export default {
   name: "UserInfo",
   components: {
-    ToastMessage
+    ToastMessage,
   },
   props: {
     user: String,
@@ -43,9 +48,9 @@ export default {
   },
   methods: {
     copy(userId) {
-    // Emit the event to parent component to handle the copy action
-    this.$emit('requestCopy', userId);
-  },
+      // Emit the event to parent component to handle the copy action
+      this.$emit("requestCopy", userId);
+    },
     joinRoom() {
       // Emit the joinRoom event with the room ID
       this.$emit("joinRoom", this.room);
@@ -59,5 +64,59 @@ export default {
 </script>
 
 <style scoped>
-/* Styles related to UserInfo component can be added here */
+
+input {
+  border-radius: 15px 0 0 15px !important;
+  border: 0 !important;
+  background-color: rgba(255, 255, 255, 0.1);
+  color: #f9f6f7;
+}
+
+input:focus {
+  box-shadow: none !important;
+  background-color: rgba(255, 255, 255, 0.05);
+  color: #f9f6f7;
+  outline: 0;
+}
+
+input::placeholder {
+  color: gray;
+}
+
+button {
+  border-radius: 0 15px 15px 0 !important;
+  border: 0 !important;
+}
+
+.inputBox {
+  margin-top: 0.4rem;
+  border-radius: 10px;
+  background-color: #23262a;
+  padding: 0.1rem 0;
+  opacity: 0.7;
+}
+
+.btn.focus,
+.btn:focus {
+  outline: 0;
+  box-shadow: none !important;
+}
+
+.clickableUserId {
+  text-decoration: underline;
+  cursor: pointer;
+  transition: color 0.3s ease;
+}
+
+.clickableUserId:hover {
+  color: #42b983;
+}
+
+.etrans {
+  -webkit-transition: all 0.2s ease-out;
+  -moz-transition: all 0.2s ease-out;
+  -ms-transition: all 0.2s ease-out;
+  -o-transition: all 0.2s ease-out;
+  transition: all 0.2s ease-out;
+}
 </style>
