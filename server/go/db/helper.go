040630e72fbdde074123ca2da809003f366dc888
Abhishek Mishra <104294494+itsmeabhishek03@users.noreply.github.com>
2023-12-26 17:15:18 +0530

    Go: Refactor DB queries
    Co-authored-by: maadi <mishraadi110@gmail.com>

--- server/go/db/helper.go
+++ server/go/db/helper.go
@@ -28,42 +28,56 @@ func randomHex(n int) (string, error) {
 }
 
 
-func NewConn() *sql.DB {
-	database, _ := sql.Open("sqlite3", FILE)
+func NewConn() (*sql.DB, error) {
+	database, err := sql.Open("sqlite3", FILE)
+	if err != nil {
+		return nil, err
+	}
 
-	statement, _ := database.Prepare(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (id TEXT PRIMARY KEY, publicKey TEXT, last_activity DATE)", TABLE))
-	statement.Exec()	
-	
-	return database
+	statement, err := database.Prepare(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (id TEXT PRIMARY KEY, publicKey TEXT, last_activity DATE)", TABLE))
+	if err != nil {
+		return nil, err
+	}
+	_, err = statement.Exec()
+	if err != nil {
+		return nil, err
+	}
+
+	return database, nil
 }
 
 
 func (db *DataBase) GetPublicKey(id string) (string, error) {
-	rows, _ := db.Query(fmt.Sprintf("SELECT publicKey FROM %s WHERE id = ?", TABLE), id)
-
 	var publicKey string
-	for rows.Next() {
-		if err := rows.Scan(&publicKey); err != nil {
-			return "", err
-		}
+	err := db.QueryRow(fmt.Sprintf("SELECT publicKey FROM %s WHERE id = ?", TABLE), id).Scan(&publicKey)
+	if err != nil {
+		return "", err
 	}
 
 	if len(publicKey) > 0 {
 		return publicKey, nil
-	} else {
-		return "", errors.New("wrong publicKey")
 	}
+	return "", errors.New("wrong publicKey")
 }
 
 
-func (db *DataBase) SaveUser(publicKey string) string {
-	id, _ := randomHex(5)
+func (db *DataBase) SaveUser(publicKey string) (string, error) {
+	id, err := randomHex(5)
+	if err != nil {
+		return "", err
+	}
+
 	date := time.Now()
+	statement, err := db.Prepare(fmt.Sprintf("INSERT INTO %s VALUES (?, ?, ?)", TABLE))
+	if err != nil {
+		return "", err
+	}
+	_, err = statement.Exec(id, publicKey, date)
+	if err != nil {
+		return "", err
+	}
 
-	statement, _ := db.Prepare(fmt.Sprintf("INSERT INTO %s VALUES (?, ?, ?)", TABLE))
-	statement.Exec(id, publicKey, date)
-
-	return id
+	return id, nil
 }
 
 
